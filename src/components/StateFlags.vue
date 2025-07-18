6367e7a2b1e0b266b9bcf7bf8756724220496ff9
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-18 11:20:20 +0200

    state flags

---
+++ src/components/StateFlags.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flags">
+    <h2>State Flags</h2>
+    <ul>
+      <li
+        v-for="flag in flags"
+        :key="flag.name"
+        :class="{ active: flag.active, inactive: !flag.active }"
+      >
+        <span>{{ flag.description }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  flags: { type: Array, required: true }
+})
+</script>
+
+<style scoped>
+.flags {
+  margin-top: 2rem;
+}
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  padding: 0.25rem 0;
+}
+.active {
+  color: green;
+}
+.inactive {
+  color: red;
+}
+</style>
