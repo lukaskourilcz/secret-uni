10d55e02fd477d58017d2753fd7742ee1794ba57
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-18 11:18:32 +0200

    auth info

--- src/components/AuthInfo.vue
+++ src/components/AuthInfo.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
+  <div class="auth-info">
+    <span>AuthInfo:</span>
     <button @click="showSecret = !showSecret">Show</button>
-    <span v-if="showSecret">{{ authInfo }}</span>
+    <span v-if="showSecret" class="secret">{{ authInfo }}</span>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-const props = defineProps(['authInfo'])
+const props = defineProps({
+  authInfo: { type: String, required: true }
+})
+
 const showSecret = ref(false)
 </script>
+
+<style scoped>
+.auth-info {
+  margin-top: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.secret {
+  font-weight: bold;
+  color: darkred;
+}
+</style>
