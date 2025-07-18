9c0098364520b6eab92cdbfc1b5abd0978b080b0
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-18 11:07:07 +0200

    json loaded into vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
-<template>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const domainData = ref(null)
+
+onMounted(async () => {
+  const res = await fetch('/domain-detail.json')
+  domainData.value = await res.json()
+})
+</script>
+
+
+<template v-if="domainData">
 
 <div>
   <h1>Neviditelna univerzita</h1>
